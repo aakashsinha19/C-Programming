0d8d760a30c3a86b7cc1610f7c65d201d4845097
Aakash Sinha <aakash19developer@gmail.com>
2017-03-12 00:47:46 +0530

    Create forLoop.cpp

---
+++ Basic Programs/forLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+     int a;
+     cout<<"Enter the Number :";
+     cin>>a;
+     for (int i = 1; i <= a; i++)
+     {
+         cout<<"Execute "<<i<<" time"<<endl;
+     }
+     return 0;
+ }
