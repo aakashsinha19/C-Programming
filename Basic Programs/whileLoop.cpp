e0c934077a844971e7927c10cff39586ef6adf45
Aakash Sinha <aakash19developer@gmail.com>
2017-03-12 00:49:02 +0530

    Create whileLoop.cpp

---
+++ Basic Programs/whileLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"Enter a number less than 10:"<<endl;
+  cin>>n;
+  while(n<=10)
+  {
+    cout<<n<<endl;
+    n++;
+  }
+  return 0;
+}
