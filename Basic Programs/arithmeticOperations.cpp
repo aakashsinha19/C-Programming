c5526d2a035cee17276e5ff3e62657bab0076c8f
Aakash Sinha <aakash19developer@gmail.com>
2017-03-12 00:42:12 +0530

    Create arithmeticOperations.cpp

---
+++ Basic Programs/arithmeticOperations.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int x,y,sum,diff,mul,div;
+  cout<<"Enter the first number :"<<endl;
+  cin>>x;
+  cout<<"Enter the second number :"<<endl;
+  cin>>y;
+  sum = x+y;
+  cout<<"Sum of x and y is = "<<sum<<endl;
+  diff = x-y;
+  cout<<"Difference of x and y is = "<<diff<<endl;
+  mul = x*y;
+  cout<<"Multiplication of x and y is = "<<mul<<endl;
+  div = x/y;
+  cout<<"Division of x and y is = "<<div<<endl;
+  return 0;
+}
